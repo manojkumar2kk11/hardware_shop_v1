a0d10464ac44bf069352ddb01f1048facf87d6b1
manojkumar2kk11 <manojkumar2kk11@gmail.com>
2025-04-27 21:32:46 +0100

    Added the profile templates static file

--- hardware_shop/urls.py
+++ hardware_shop/urls.py
@@ -18,11 +18,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import handler404
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
-     path('checkout/', include('checkout.urls')),
+    path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'hardware_shop.views.handler404'
