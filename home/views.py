3692bd4aacf057f90e7fb80ad71d5b5ee357b115
manojkumar2kk11 <manojkumar2kk11@gmail.com>
2025-08-03 11:34:33 +0100

    Add news letter signup

--- home/views.py
+++ home/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import NewsletterForm
 
 def index(request):
-    """ A view to return the index page """
-    
-    return render(request, 'home/index.html')
+    """ A view to return the index page and handle newsletter form """
+    form = NewsletterForm()
+
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Subscribed successfully!')
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'home/index.html', context)
+
+
+def subscribe(request):
+    """ If you want to have a dedicated subscribe page """
+    form = NewsletterForm()
+
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Subscribed successfully!')
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'home/subscribe.html', context)
