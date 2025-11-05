3b108f411e330d8ec6c79843f4f13cdd702af434
manojkumar2kk11 <manojkumar2kk11@gmail.com>
2025-11-05 16:40:58 +0530

    added 404 page not found err

--- hardware_shop/urls.py
+++ hardware_shop/urls.py
@@ -15,22 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),   # <-- add this line
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# keep your handler declarations if present
 handler404 = 'hardware_shop.views.handler404'
--- hardware_shop/views.py
+++ hardware_shop/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-
-def handler404(request, exception):
-    """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
+def handler404(request, exception=None):
+    """Custom 404 handler used by Django when DEBUG=False."""
+    return render(request, "404.html", status=404)
