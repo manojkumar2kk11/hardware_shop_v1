e15342fce5df5deeb72e3f5a63edda4972189e81
Arunkumar Gunasekaran <manojkumar2kk11@gmail.com>
2025-11-10 14:47:22 +0530

    added the faq and contact us page

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('faq/', views.faq, name='faq'),
+    path('contact/', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ def index(request):
     """ A view to return the index page """
     
     return render(request, 'home/index.html')
+
+
+def faq(request):
+    """ Frequently Asked Questions page """
+    return render(request, 'home/faq.html')
+
+
+def contact(request):
+    """ Contact Us page """
+    return render(request, 'home/contact.html')
