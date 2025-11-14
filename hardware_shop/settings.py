a7c7113f8f3b46db46d79034dd480da4399a6ccf
manojkumar2kk11 <manojkumar2kk11@gmail.com>
2025-11-14 01:57:51 +0000

    Install the whitenoise and add in the settings

--- hardware_shop/settings.py
+++ hardware_shop/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -179,7 +180,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+if 'USE_AWS' not in os.environ:
+    STATICFILES_STORAGE = (
+        'whitenoise.storage.'
+        'CompressedManifestStaticFilesStorage'
+    )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
